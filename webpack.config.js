008385ec364bec0a9b7d2fdadb0fdbc003532ef6
chikara <chenhongtu@51xianqu.net>
2017-01-09 16:51:08 +0800

    Make webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path'),
+    fs = require('fs'),
+    webpack = require('webpack'),
+    autoprefixer = require('autoprefixer'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: {
+        'canvas-playground/viscosity/bundle': './docs/canvas-playground/viscosity',
+        'css-playground/lovely-dog/bundle': './docs/css-playground/lovely-dog',
+        'css-playground/rotate-cube/bundle': './docs/css-playground/rotate-cube',
+        'css-playground/rotate-shadow/bundle': './docs/css-playground/rotate-shadow'
+    },
+    output: {
+        path: './docs',
+        filename: '[name].js',
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'stage-0'],
+                plugins: ['transform-runtime', 'add-module-exports']
+            }
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
+        }, {
+            test: /\.(jpg|png|gif|webp)$/,
+            loader: 'url?limit=8000'
+        }, {
+            test: /\.json$/,
+            loader: 'json'
+        }]
+    },
+    postcss: [autoprefixer({browsers: ['> 5%']})],
+    resolve: {extensions: ['', '.js', '.json', '.scss']},
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new ExtractTextPlugin('[name].css')
+    ]
+}
